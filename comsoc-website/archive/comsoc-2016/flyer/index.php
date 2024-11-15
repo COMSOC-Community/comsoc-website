0a47777801f5e0854a42570436662be59ef3131c
Simon Rey <reysimon@orange.fr>
2024-11-15 17:20:17 +0100

    COMSOC 2016

---
+++ comsoc-website/archive/comsoc-2016/flyer/index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>COMSOC-2016: Sixth International Workshop on Computational Social Choice</title>
+<link rel="stylesheet" href="web/comsoc.css" type="text/css">
+</head>
+
+<body>
+
+<center>
+
+<!-- top banner starts here -->
+<table border="0" cellspacing="0" cellpadding="0" width="850px">
+<tr><td background="images/toulouse.jpg" height="220px" valign="top">
+
+  <div id="top">
+  Sixth International Workshop on Computational Social Choice<br>
+  Toulouse, France, 22&ndash;24 June 2016
+  </div>
+</td></tr>
+</table>
+<!-- top banner ends here -->
+
+<table border="0" cellspacing="0" cellpadding="0" width="850px">
+<tr><td height="2px"></td></tr>
+<tr valign="top">
+
+<!-- menu starts here -->
+<td width="175px" align="left">
+    <div id="menu">
+  <a href="index.html">COMSOC-2016</a>
+  <!--<a href="program.html">Program</a>-->
+  <a href="acc.html">Accepted Papers</a>
+  <a href="program.html">Program</a>
+  <!--<a href="proceedings.html">Workshop Notes</a> -->
+  <a href="invited.html">Invited Speakers</a>
+  <!-- <a href="tutorial.html">LogICCC Tutorial Day</a> -->
+ <a href="pictures.html">Pictures</a>
+ <a href="pc.html">Committees</a>
+  <a href="cfp.html">Call for Papers</a>
+  <a href="dates.html">Important Dates</a>
+  <a href="industry.html">Industry Day</a>
+  <a href="submission.html">Paper submission</a>
+  <a href="registration.html">Registration</a>
+  <a href="location.html">Location</a>
+  <a href="travel.html">Travel and Accommodation</a>
+  <a href="sponsors.html">Sponsors</a>
+  <a href="http://www.illc.uva.nl/COMSOC/workshops.html" target="_blank">Previous COMSOCs</a>
+  <a href="contact.html">Contact</a>
+  </div>
+</td>
+
+
+<td>&nbsp;&nbsp;&nbsp;&nbsp;</td>
+
+<td width="100%">
+<!-- page content starts here -->
+
+
+<h1>About COMSOC-2016 ...</h1>
+
+
+<p>Computational social choice is a rapidly growing discipline at the 
+interface  of  social  choice  theory  and  computer  science.  It  is 
+concerned with the application  of  computational  techniques  to  the 
+study of social choice mechanisms, and with the integration of  social 
+choice paradigms into computing.</p>
+
+<p>The <b>Sixth International Workshop on Computational Social Choice
+(COMSOC-2016) will take place on June 22&ndash;24, 2016, in Toulouse, France</b>.
+It will be hosted by the 
+<a href="http://www.irit.fr" target="_blank">Institut de Recherche en Informatique de Toulouse (IRIT)</a>
+ of the
+<a href="http://www.univ-toulouse.fr" target="_blank">University of Toulouse</a>.
+The aim of the workshop is to bring together different communities:
+computer scientists interested  in  computational  issues  in 
+social choice; people working in artificial  intelligence  and
+multiagent systems who are using  ideas  from  social  choice  to  organize 
+societies of artificial software agents; logicians interested  in  the 
+logic-based specification and analysis of  social  procedures; 
+computer science theorists analyzing algorithmic properties of social phenomena;
+and last but not least people  coming  from  social  choice 
+theory itself: economists, mathematicians, and political scientists.</p>
+
+<p><b>
+COMSOC-2016 is temporally co-located with <a href="http://www.nek.lu.se/en/scw16" target="_blank">The 13th Meeting of the Society for Social Choice and Welfare</a>, which will take place on June 28th&ndash;July 1st at Lund University, Sweden.
+</p></b>
+
+<br>
+
+<h1>Call for Papers </h1>
+<p>
+Submissions of papers describing original or recently published work on all aspects of computational social choice are invited. Papers will have to be submitted electronically via the workshop webpage, and accepted papers will be collected in informal workshop notes. COMSOC-2016 will also include a poster session, with selection based on abstract submitted by emails. A full call for papers is available on the COMSOC-2016 webpage: <a href="http://www.irit.fr/COMSOC-2016/cfp.php">www.irit.fr/COMSOC-2016/cfp.php</a>
+</p>
+
+<p>
+For additional information please consult the <b>workshop webpage</b>: <a href="http://www.irit.fr/COMSOC-2016">www.irit.fr/COMSOC-2016</a>
+</p>
+
+<br>
+
+<h1>Sponsors </h1>
+
+<center>
+<table border="0">
+<tr><td height="20px"></td></tr>
+<tr>
+  <td>
+   <center>
+   <a href="http://www.illc.uva.nl/COST-IC1205"><img src="images/cost-logo-grey.png" width="120px"><a>
+   </center>
+  </td>
+   <td width=20px>
+  </td>
+
+  
+   <td>
+   <a href="http://ec.europa.eu/programmes/horizon2020/"><img src="images/eu-flag.png" width="100px"></a>
+  </td>
+   <td width=20px>
+  </td>
+  
+   <td>
+   <a href="http://www.irit.fr"><img src="images/irit.png" width="220px"></a>
+  </td>
+</tr>
+
+<tr>
+  <td>
+   <a href="http://www.iast.fr"><img src="images/iast.png" width="70px"></a>
+  </td>
+
+   <td width=20px>
+  </td>
+
+
+   <td>
+   <a href="http://www.ut-capitole.fr/"><img src="images/ut1.png" width="97px"></a>
+  </td>
+
+   <td width=20px>
+  </td>
+
+  <td>
+  <a href="http://www.univ-tlse3.fr/"><img src="images/ut3.png" width="220px"></a>  
+  </td>
+
+
+</tr>
+</table>
+</center>
